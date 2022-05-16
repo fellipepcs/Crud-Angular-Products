135688251d24393ecdfb9c8717a5e36aeecb7647
fellipepcs <fellipy.02@hotmail.com>
2022-05-16 12:27:27 -0300

    Adicionando service de products

---
+++ src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,35 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: 'Produto de teste',
+    price: 125.98
+  }
+
+  constructor(private productService: ProductService, private router: Router) {
+
+  }
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/products'])
+  }
+}
